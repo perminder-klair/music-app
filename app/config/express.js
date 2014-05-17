935db04f5afe24e090638689139360d8010089e4
parminder <perminder.klair@gmail.com>
2014-05-17 11:37:34 +0100

    removed favicon

--- app/config/express.js
+++ app/config/express.js
@@ -35,7 +35,7 @@ module.exports = function (app) {
 
     if ('production' === env) {
         app.use(compression());
-        app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
+        //app.use(favicon(path.join(config.root, 'favicon.ico')));
     }
 
     app.use(express.static(path.join(config.root, 'app')));
