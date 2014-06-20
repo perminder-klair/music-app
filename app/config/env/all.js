ac4573fecedbe6be7150b950d499fb685ed7301f
parminder <perminder.klair@gmail.com>
2014-06-20 22:44:16 +0100

    made work with latest api changes

--- app/config/env/all.js
+++ app/config/env/all.js
@@ -7,8 +7,8 @@ var rootPath = path.normalize(__dirname + '/../../..');
 module.exports = {
     root: rootPath,
     port: process.env.PORT || 5000,
-    apiUrl: 'http://imp3songs.dev/api/',
-    //apiUrl: 'http://api2.imp3songs.com/api/',
+    //apiUrl: 'http://imp3songs.dev/api/',
+    apiUrl: 'http://api2.imp3songs.com/api/',
     apiToken: '1234',
     siteTitle: 'iMp3Songs.in'
 };
