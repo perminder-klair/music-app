1c9fd64197c4920d7029f852be84e4c458db1564
perminder-klair <klair.di.sardari@gmail.com>
2014-07-22 10:36:04 +0000

    added sync latest albums on home page

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var path = require('path'),
+    async = require('async'),
     request = require('request'),
     config = require('../config/config');
 
@@ -25,23 +26,56 @@ exports.partials = function(req, res) {
  * Send our single page app
  */
 exports.index = function(req, res) {
-    var options = {
-        url: config.apiUrl + 'genres',
-        qs: {'access-token': config.apiToken}
-    };
-    request(options, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            //console.log(body);
-            var genres = JSON.parse(body);
+    
+    async.parallel([
+	function(next) {
+        //get latest albums
+        var options = {
+            url: config.apiUrl + 'albums',
+            qs: {
+                'access-token': config.apiToken,
+                'limit': 10,
+                'expand': 'artist'
+            }
+        };
+        
+        request(options, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                //console.log(body);
 
-            res.render('index', {
-                title: 'Home - ' + config.siteTitle,
-                genres: genres
-            });
-        } else {
-            console.log("Error rendering requested page");
-            res.status(404);
-            res.send(404);
-        }
-    });
+                next(null, body);
+            } else {
+                console.log("Error rendering requested page");
+                res.status(404);
+                res.send(404);
+            }
+        });
+    },
+    function(next) {
+        //get genres
+        var options = {
+            url: config.apiUrl + 'genres',
+            qs: {'access-token': config.apiToken}
+        };
+        request(options, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                //console.log(body);
+                var genres = JSON.parse(body);
+                
+                next(null, body);
+            } else {
+                console.log("Error rendering requested page");
+                res.status(404);
+                res.send(404);
+            }
+        });
+    }], function(err, results) {
+        // results is [firstData, secondData]
+        res.render('index', {
+            title: 'Home - ' + config.siteTitle,
+            albums: JSON.parse(results[0]),
+            genres: JSON.parse(results[1])
+        });
+    });    
+    
 };
